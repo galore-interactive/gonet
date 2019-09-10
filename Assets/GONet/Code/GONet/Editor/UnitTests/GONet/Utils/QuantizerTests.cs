dbb9e9a7733bde3dbc79f5654ca9e6d24c878063
Shaun Sheppard <shasheppard@gmail.com>
2019-09-10 23:46:58 +0000

    updated license text

--- Assets/GONet/Code/GONet/Editor/UnitTests/GONet/Utils/QuantizerTests.cs
+++ Assets/GONet/Code/GONet/Editor/UnitTests/GONet/Utils/QuantizerTests.cs
@@ -1,7 +1,7 @@
-﻿/* Copyright (c) 2019 Galore Interactive LLC - All Rights Reserved
+﻿/* GONet (TM pending, serial number 88592370), Copyright (c) 2019 Galore Interactive LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Shaun Sheppard <shasheppard@gmail.com>, June 2019
+ * 
  *
  * Authorized use is explicitly limited to the following:	
  * -The ability to view and reference source code without changing it
@@ -9,8 +9,8 @@
  * -The ability to distribute products based on original sources for non-commercial purposes, whereas this license must be included if source code provided in said products
  * -The ability to commercialize products built on original source code, whereas this license must be included if source code provided in said products
  * -The ability to modify source code for local use only
- * -The ability to distribute products based on modified sources for non-commercial purposes, whereas this license must be included if source code provided in said products
- * -The ability to commercialize products built on modified source code, whereas this license must be included if source code provided in said products
+ * -The ability to distribute products based on modified sources for non-commercial purposes, whereas this license must be included if source code provided in said products and whereas the products are interactive multi-player video games and cannot be viewed as a product competitive to GONet
+ * -The ability to commercialize products built on modified source code, whereas this license must be included if source code provided in said products and whereas the products are interactive multi-player video games and cannot be viewed as a product competitive to GONet
  */
 
 using NUnit.Framework;
