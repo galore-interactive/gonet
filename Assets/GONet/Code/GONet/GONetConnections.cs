cf7dd8a50a58f819de09e4735e278f9cc1afc91f
Shaun Sheppard <shasheppard@gmail.com>
2019-09-17 00:07:58 +0000

    updated license text again...mixed up some non-commercial and commercial lines....oops

--- Assets/GONet/Code/GONet/GONetConnections.cs
+++ Assets/GONet/Code/GONet/GONetConnections.cs
@@ -7,9 +7,9 @@
  * -The ability to view and reference source code without changing it
  * -The ability to enhance debugging with source code access
  * -The ability to distribute products based on original sources for non-commercial purposes, whereas this license must be included if source code provided in said products
- * -The ability to commercialize products built on original source code, whereas this license must be included if source code provided in said products
+ * -The ability to commercialize products built on original source code, whereas this license must be included if source code provided in said products and whereas the products are interactive multi-player video games and cannot be viewed as a product competitive to GONet
  * -The ability to modify source code for local use only
- * -The ability to distribute products based on modified sources for non-commercial purposes, whereas this license must be included if source code provided in said products and whereas the products are interactive multi-player video games and cannot be viewed as a product competitive to GONet
+ * -The ability to distribute products based on modified sources for non-commercial purposes, whereas this license must be included if source code provided in said products
  * -The ability to commercialize products built on modified source code, whereas this license must be included if source code provided in said products and whereas the products are interactive multi-player video games and cannot be viewed as a product competitive to GONet
  */
 
