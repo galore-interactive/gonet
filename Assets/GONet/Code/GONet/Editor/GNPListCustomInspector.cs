a81e92427ed718d71eae850bf8f3358d80bf984e
Shaun Sheppard <shasheppard@gmail.com>
2019-09-21 20:53:10 +0000

    oops...forgot to add header license text in new files from last commit

--- Assets/GONet/Code/GONet/Editor/GNPListCustomInspector.cs
+++ Assets/GONet/Code/GONet/Editor/GNPListCustomInspector.cs
@@ -1,4 +1,18 @@
-﻿
+﻿/* GONet (TM pending, serial number 88592370), Copyright (c) 2019 Galore Interactive LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential, email: contactus@unitygo.net
+ * 
+ *
+ * Authorized use is explicitly limited to the following:	
+ * -The ability to view and reference source code without changing it
+ * -The ability to enhance debugging with source code access
+ * -The ability to distribute products based on original sources for non-commercial purposes, whereas this license must be included if source code provided in said products
+ * -The ability to commercialize products built on original source code, whereas this license must be included if source code provided in said products and whereas the products are interactive multi-player video games and cannot be viewed as a product competitive to GONet
+ * -The ability to modify source code for local use only
+ * -The ability to distribute products based on modified sources for non-commercial purposes, whereas this license must be included if source code provided in said products
+ * -The ability to commercialize products built on modified source code, whereas this license must be included if source code provided in said products and whereas the products are interactive multi-player video games and cannot be viewed as a product competitive to GONet
+ */
+
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
